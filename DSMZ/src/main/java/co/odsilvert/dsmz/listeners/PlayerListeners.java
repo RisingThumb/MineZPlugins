2f80a5ecff677388bcb1489e0a2cdcf1425ee3a8
Malod219 <perf3ctodst@gmail.com>
2018-12-22 07:52:05 +0000

    Added listeners architecture
    Listeners properly done with alright architecture

--- DSMZ/src/main/java/co/odsilvert/dsmz/listeners/PlayerListeners.java
+++ DSMZ/src/main/java/co/odsilvert/dsmz/listeners/PlayerListeners.java
@@ -1,10 +1,26 @@
 package co.odsilvert.dsmz.listeners;
 
-import co.odsilvert.dsmz.main.DSMZ;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 
 public class PlayerListeners implements Listener {
-
-    private DSMZ plugin = DSMZ.getPlugin();
-
+    
+	// All singletons here
+    private SugarSpeed sugarspeed = new SugarSpeed();
+    private PotionsRemove potionsremove = new PotionsRemove();
+    
+    
+	@EventHandler(priority=EventPriority.HIGH)
+    public void onPlayerInteract(PlayerInteractEvent event){
+    	// All actions for that event would be listed here
+		sugarspeed.action(event);
+    }
+	
+    @EventHandler
+    public void onPotionConsume(PlayerItemConsumeEvent event) {
+    	potionsremove.action(event);
+    }
 }
---
+++ DSMZ/src/main/java/co/odsilvert/dsmz/listeners/PotionsRemove.java
@@ -0,0 +1,46 @@
+package co.odsilvert.dsmz.listeners;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import co.odsilvert.dsmz.main.DSMZ;
+
+public class PotionsRemove {
+	
+	private DSMZ plugin = DSMZ.getPlugin();
+
+	public void action(PlayerItemConsumeEvent event) {
+        Player player = event.getPlayer();
+        ItemStack item = event.getItem();
+        int itemSlot = player.getInventory().getHeldItemSlot();
+
+        switch (item.getType()) {
+            case POTION:
+                deleteConsumedItem(player, Material.GLASS_BOTTLE, itemSlot);
+                break;
+            case MILK_BUCKET:
+                deleteConsumedItem(player, Material.BUCKET, itemSlot);
+                break;
+            default:
+            	break;
+        }
+	}
+    private void deleteConsumedItem(Player player, Material itemType, int slot) {
+        BukkitRunnable deleteItem = new BukkitRunnable() {
+            @Override
+            public void run() {
+                ItemStack item = player.getInventory().getItem(slot);
+
+                if (item.getType().equals(itemType)) {
+                    player.getInventory().setItem(slot, null);
+                }
+            }
+        };
+
+        deleteItem.runTaskLater(plugin, 1L);
+}
+
+}
