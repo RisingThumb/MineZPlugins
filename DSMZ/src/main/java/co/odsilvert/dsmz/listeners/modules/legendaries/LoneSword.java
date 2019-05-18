bb93ae5b78346bea129787c6a876ae99962f5366
KevlarBullet <ian.kellogg001@gmail.com>
2019-05-18 01:30:59 -0800

    Tested and fixed the current Lone Sword implementation
    Has a known bug of throwing a random (seemingly harmless) error when the sword is removed from the inventory

--- DSMZ/src/main/java/co/odsilvert/dsmz/listeners/modules/legendaries/LoneSword.java
+++ DSMZ/src/main/java/co/odsilvert/dsmz/listeners/modules/legendaries/LoneSword.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import com.google.inject.Inject;
@@ -16,41 +17,56 @@ import co.odsilvert.dsmz.main.DSMZ;
 
 @Singleton
 public class LoneSword {
-	private DSMZ plugin;
-	
+	@Inject private DSMZ plugin;
+
 	@Inject PlayerStatusHandler playerStatusHandler;
-	
+
 	private HashMap<UUID, Boolean> loneSwordUsed = new HashMap<>();
-	
-	public void action(PlayerInteractEvent event) {
-		final Player player = event.getPlayer();
-		
-		if (!loneSwordUsed.containsKey(player.getUniqueId())) {
-			loneSwordUsed.put(player.getUniqueId(), false);
-		}
-		String itemName = player.getEquipment().getItemInMainHand().getItemMeta().getDisplayName();
-		switch(itemName) {
-		case "Lone Sword":
-			loneSwordAction(player);
-			break;
-		default:
-			break;
-		}
-	}
-	
-	private void loneSwordAction(final Player player) {
-		BukkitRunnable resetTask = new BukkitRunnable() {
-			public void run() {
-				loneSwordUsed.put(player.getUniqueId(), false);
-			}
-		};
-		if (player.getEquipment().getItemInMainHand().getItemMeta().getDisplayName().equals("Lone Sword") && !loneSwordUsed.get(player.getUniqueId())) {
-			player.setHealth(player.getHealth()+4);
+
+//	public void action(PlayerInteractEvent event) {
+//		final Player player = event.getPlayer();
+//
+//		if (!loneSwordUsed.containsKey(player.getUniqueId())) {
+//			loneSwordUsed.put(player.getUniqueId(), false);
+//		}
+//		String itemName = player.getEquipment().getItemInMainHand().getItemMeta().getDisplayName();
+//		switch(itemName) {
+//		case "Lone Sword":
+//			loneSwordAction(player);
+//			break;
+//		default:
+//			break;
+//		}
+//	}
+
+	public void action(final Player player) {
+
+		// By this point, the item the player's holding is guaranteed to be a Lone Sword
+//		if (player.getEquipment().getItemInMainHand().getItemMeta().getDisplayName().equals("Lone Sword") && !loneSwordUsed.get(player.getUniqueId())) {
+
+		if (!loneSwordUsed.containsKey(player.getUniqueId()) || !loneSwordUsed.get(player.getUniqueId())) {
+			ItemStack loneSword = player.getEquipment().getItemInMainHand();
+
+			// Can't set health to a value above 20; throws an exception.
+			player.setHealth(player.getHealth() + Math.min(4, 20 - player.getHealth()));
 			player.sendMessage(ChatColor.GREEN+"You healed yourself with your lone sword!");
-			player.getEquipment().getItemInMainHand().setDurability((short) (player.getEquipment().getItemInMainHand().getDurability()-10));
-			resetTask.runTaskLater(this.plugin, 1200L);
-		}
-		else {
+
+			// Durability is backwards. Might be referencing durability lost instead of durability left.
+			loneSword.setDurability((short) (player.getEquipment().getItemInMainHand().getDurability() + 10));
+			// Remove lone sword if damage exceeds 250 (iron sword max durability) to prevent getting item with negative durability
+			if (loneSword.getDurability() > loneSword.getType().getMaxDurability()) {
+				player.getInventory().setItemInMainHand(null);
+			}
+			loneSwordUsed.put(player.getUniqueId(), true);
+
+			// No point creating the task if it's not going to be run
+			BukkitRunnable resetTask = new BukkitRunnable() {
+				public void run() {
+					loneSwordUsed.put(player.getUniqueId(), false);
+				}
+			};
+			resetTask.runTaskLater(this.plugin, 200L);
+		} else {
 			player.sendMessage(ChatColor.RED+"You need to wait before using this item again!");
 		}
 	}
