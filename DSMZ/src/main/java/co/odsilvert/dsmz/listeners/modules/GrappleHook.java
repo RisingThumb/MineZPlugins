8953cb26844211abd82e931c3f19c0892a77d45f
Malod219 <perf3ctodst@gmail.com>
2019-06-20 23:05:31 +0100

    Added checks for grapple hook name

--- DSMZ/src/main/java/co/odsilvert/dsmz/listeners/modules/GrappleHook.java
+++ DSMZ/src/main/java/co/odsilvert/dsmz/listeners/modules/GrappleHook.java
@@ -17,35 +17,38 @@ public class GrappleHook {
 		Player player = event.getPlayer();
 		ItemStack item = player.getEquipment().getItemInMainHand();
 		
-		if (item.getType().equals(Material.FISHING_ROD)) {
-			List<Entity> nearby = player.getNearbyEntities(30,30,30);
-			Entity hook = null;
-			for (Entity e : nearby) {
-				if (e.getType().equals(EntityType.FISHING_HOOK)) {
-					hook = e;
-					break;
+		String itemName = item.getItemMeta().getDisplayName();
+
+		if (itemName.equals("Grappling Hook")) {
+			if (item.getType().equals(Material.FISHING_ROD)) {
+				List<Entity> nearby = player.getNearbyEntities(30,30,30);
+				Entity hook = null;
+				for (Entity e : nearby) {
+					if (e.getType().equals(EntityType.FISHING_HOOK)) {
+						hook = e;
+						break;
+					}
 				}
+				if (hook == null) {
+					return;
+				}
+				Vector hookVelocity = hook.getVelocity();
+				if (!hookVelocity.equals(new Vector(0,0,0))) {
+					player.sendMessage(ChatColor.RED+"Your hook is not attached to a block.");
+					return;
+				}
+				Location hookPosition = hook.getLocation();
+				Location playerPosition = player.getEyeLocation();
+				Location distanceOf2Points = hookPosition.subtract(playerPosition);
+				// We get distance of the 2 points as location. Location is X Y and Z data like a 3D Vector
+				if (distanceOf2Points.getY()<0) {
+					player.sendMessage(ChatColor.RED+"Your hook is not attached high enough");
+					return;
+				}
+				Vector playerVelocity = new Vector(distanceOf2Points.getX()/5, distanceOf2Points.getY()/5, distanceOf2Points.getZ()/5);
+				player.setVelocity(playerVelocity);
+				item.setDurability((short) (player.getEquipment().getItemInMainHand().getDurability() + 9));
 			}
-			if (hook == null) {
-				return;
-			}
-			Vector hookVelocity = hook.getVelocity();
-			if (!hookVelocity.equals(new Vector(0,0,0))) {
-				player.sendMessage(ChatColor.RED+"Your hook is not attached to a block.");
-				return;
-			}
-			Location hookPosition = hook.getLocation();
-			Location playerPosition = player.getEyeLocation();
-			Location distanceOf2Points = hookPosition.subtract(playerPosition);
-			// We get distance of the 2 points as location. Location is X Y and Z data like a 3D Vector
-			if (distanceOf2Points.getY()<0) {
-				player.sendMessage(ChatColor.RED+"Your hook is not attached high enough");
-				return;
-			}
-			Vector playerVelocity = new Vector(distanceOf2Points.getX()/5, distanceOf2Points.getY()/5, distanceOf2Points.getZ()/5);
-			player.setVelocity(playerVelocity);
-			item.setDurability((short) (player.getEquipment().getItemInMainHand().getDurability() + 9));
 		}
 	}
-	
 }
